c9b03f996b23c1372b1e1c189c59f7479017e3cc
caitleigh <caitleigh27@gmail.com>
2021-02-23 13:28:50 -0800

    initial styling for cards complete.

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Grid from '@material-ui/core/Grid'
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button
@@ -10,29 +10,37 @@ import { IoMdStopwatch } from 'react-icons/io';
 
 import { TiArrowLoop } from 'react-icons/ti'
 
+//css
+import './Card.css'
+
 
 export default function MediaCard(props) {
 
     return (
+        <Grid item>
 
-        <Card>
-            <CardImg
-                top width="100%"
-                className='headerImg'
-                src={props.headerImage}
-                alt={props.title}
-                title={props.title}
-            />
-            <CardBody>
-                <CardTitle>{props.title}</CardTitle>
-                <img src={props.trainer} alt='trainer headshot' />
-                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                    <IoMdStopwatch /> {props.time} <TiArrowLoop />{props.distance}
-                </CardSubtitle>
-                <Button size="large" color="primary">
-                    View Details
+            <Card className='card-container'>
+                <CardImg
+                    top width="100%"
+                    className='header-img'
+                    src={props.headerImage}
+                    alt={props.title}
+                    title={props.title}
+                />
+                <CardBody className='card-body-container'>
+                    <div className='title-container'>
+                        <CardTitle className="card-title">{props.title}</CardTitle>
+                        <img src={props.trainer} alt='trainer headshot' />
+                    </div>
+                    <CardSubtitle className="subtitle">
+                        <IoMdStopwatch /> {props.time} <TiArrowLoop />{props.distance}
+                    </CardSubtitle>
+                    <Button size="large" color="primary">
+                        View Details
         </Button>
-            </CardBody>
-        </Card>
+                </CardBody>
+            </Card>
+        </Grid>
+
     );
 }
