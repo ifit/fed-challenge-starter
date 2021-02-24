633bbe5e05667162f384fcd66a89b6de92017161
caitleigh <caitleigh27@gmail.com>
2021-02-24 14:36:21 -0800

    included overlay on series workouts and finished cleaning up code. ready to deploy

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Grid from '@material-ui/core/Grid'
 import {
     Card, CardImg, CardBody,
@@ -9,7 +8,7 @@ import {
 //Icons
 import { IoMdStopwatch } from 'react-icons/io';
 import { TiArrowLoop } from 'react-icons/ti'
-// import { MdPlaylistPlay } from 'react-icons/md'
+import { MdPlaylistPlay } from 'react-icons/md'
 
 //css
 import './Card.css'
@@ -18,24 +17,32 @@ import './Card.css'
 export default function MediaCard(props) {
 
     return (
-        <Grid item>
+        <Grid>
 
             <Card className={props.active === 'active' ? 'card-container active' : 'card-container'}
             >
-                <CardImg
-                    top width="100%"
-                    className='header-img'
-                    src={props.headerImage}
-                    alt={props.title}
-                    title={props.title}
-                />
+                <div className='header-img-container'>
+                    <CardImg
+                        top width="100%"
+                        className='header-img'
+                        src={props.headerImage}
+                        alt={props.title}
+                        title={props.title}
+                    />
+                    <div className={props.type === 'series' ? 'series-overlay' : 'hide'}>
+                        <h3>{props.workouts}</h3>
+                        <p>workouts</p>
+                        <MdPlaylistPlay className='series-icon' />
+                    </div>
+                </div>
                 <CardBody className='card-body-container'>
                     <div className='title-container'>
                         <CardTitle className="card-title">{props.title}</CardTitle>
                         <img src={props.trainer} alt='trainer headshot' />
                     </div>
                     <div className={props.type === 'class' ? 'subtitle' : 'hide'}>
-                        <IoMdStopwatch /> {props.time} <TiArrowLoop /> {props.distance} M
+                        <IoMdStopwatch /> {props.time} <TiArrowLoop />
+                        {props.distance} M
                     </div>
                     <Button className={props.active === 'active' ? '' : 'hide'}>
                         View Details </Button>
